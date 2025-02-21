fa61873c3b94e1005e6a2648efbba14f082bcc44
neil <neil.sheahan95@gmail.com>
2025-02-21 12:27:13 +0000

    updated entity to keep track of its location
    also moved more of the entity createion logic from Entity Generator into the createEntity function of Source

--- hepyaestus/Entity.py
+++ hepyaestus/Entity.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Optional
 
 from hepyaestus.baseClasses import BaseObject, CoreObject
+from hepyaestus.EventData import EventData
 
 if TYPE_CHECKING:
     from hepyaestus.Line import Line
@@ -22,4 +23,12 @@ class Entity(BaseObject):
         self.printTrace(create=None)
         self.creationTime = self.env.now
         self.startTime = self.env.now
-        self.currentStation = currentStation if currentStation else None
+        self.stations: list[tuple[float, Optional[CoreObject]]] = [
+            (self.env.now, currentStation)
+        ]
+        self.currentStation: Optional[CoreObject] = currentStation
+
+    def updateStation(self, station: CoreObject) -> None:
+        self.printTrace(enter=EventData(caller=station, time=self.env.now))
+        self.currentStation = station
+        # ? will I need a list of stations visited for output tracing
