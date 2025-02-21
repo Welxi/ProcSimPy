cb0077559645507dd476115d2cdc2f8088e9a916
neil <neil.sheahan95@gmail.com>
2025-02-21 12:29:02 +0000

    Line functions for controling trace logic

--- hepyaestus/Line.py
+++ hepyaestus/Line.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from hepyaestus.baseClasses import CoreObject
+    from hepyaestus.baseClasses import BaseObject, CoreObject
     from simpy import Environment
 
     # from hepyaestus.Exit import Exit
@@ -21,8 +21,9 @@ class Line:
         # self.QueueList: list[Queue] = [objectList[1]]
         # self.SourceList: list[Source] = [objectList[0]]
 
-        self.settings: dict[str, int | bool] = {
-            'traceIsOn': True  # checked by baseObject before calling printTrace
+        self.settings: dict[str, bool | None | list[BaseObject]] = {
+            'traceIsOn': True,  # checked by baseObject before calling printTrace
+            'printFiltering': None,  # print only those in list if not None
         }
 
     def initialize(self, env: Environment) -> None:
@@ -33,3 +34,18 @@ class Line:
 
     def turnTraceingOff(self) -> None:
         self.settings['traceIsOn'] = False
+
+    def canTrace(self, object: BaseObject) -> bool:
+        if not self.settings['traceIsOn']:
+            return False
+        if self.settings['printFiltering'] is not None:
+            assert type(self.settings['printFiltering']) is list
+            return object in self.settings['printFiltering']
+        return True
+
+    def filterTrace(self, filter: BaseObject):
+        if self.settings['printFiltering'] is None:
+            self.settings['printFiltering'] = [filter]
+        else:
+            assert type(self.settings['printFiltering']) is list
+            self.settings['printFiltering'].append(filter)
