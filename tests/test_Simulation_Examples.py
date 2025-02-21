43dadd71aa3dc1ed615834e1159ac42d94f0f0ea
neil <neil.sheahan95@gmail.com>
2025-02-21 12:18:10 +0000

    tests for Parallel-Machines

--- tests/test_Simulation_Examples.py
+++ tests/test_Simulation_Examples.py
@@ -4,4 +4,14 @@ def test_single_server():
     results = main(test=True)
     assert results is not None
     assert results['parts'] == 2880
-    # assert 49.99 < results['working_ratio'] < 50.01
+    assert 49.99 < results['working_ratio'] < 50.01
+
+
+def test_parallel_server():
+    from ParallelMachines import main
+
+    results = main(test=True)
+    assert results is not None
+    assert results['parts'] == 2880
+    assert 23.09 < results['working_ratio_M1'] < 23.1
+    assert 26.9 < results['working_ratio_M2'] < 26.91
