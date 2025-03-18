973c4fe735215f61cf7bbdf6f714f04b967b733a
neil <neil.sheahan95@gmail.com>
2025-03-18 16:30:16 +0000

    add dep to requirements for read the docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser', 'sphinx.ext.autodoc', 'autodoc2']
+extensions = ['myst_parser', 'autodoc2']
 
 templates_path = ['_templates']
 exclude_patterns = []
