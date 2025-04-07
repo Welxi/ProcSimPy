978b08a348aa86e160e8ec3fe38535d7267b5036
neil <neil.sheahan95@gmail.com>
2025-04-07 16:17:54 +0100

    Machine can now be Interrupted with a failure that is repaired by a repair technician

--- docs/source/Reference/Entity.md
+++ docs/source/Reference/Entity.md
@@ -1,4 +1,4 @@
 # Entity
-Goal: Abstraction for the thing the process it acting apon
+Goal: 
 
 Created in Source this component tracks it creation time and when it can be handed over to each node
---
+++ hepyaestus/RepairTechnician.py
@@ -0,0 +1,6 @@
+from hepyaestus.Resource import ResourceObject
+
+
+class RepairTechnician(ResourceObject):
+    def __init__(self, id: str, name: str, *, capacity: int = 1) -> None:
+        super().__init__(id, name, capacity=capacity)
