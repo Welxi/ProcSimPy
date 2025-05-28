7c76e73c8192fcbcbf996e82bfcff97c5fb9c340
neil <neil.sheahan95@gmail.com>
2025-05-28 09:28:49 +0100

    Specfic Node Types

--- procsimpy/Exit.py
+++ procsimpy/Exit.py
@@ -1,38 +1,75 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 
 from procsimpy.Entity import Entity
-from procsimpy.EventData import EventData
-from procsimpy.StoreNode import StoreNode
+from procsimpy.EventData import ReceiveEvent
+from procsimpy.Node import Node
+from simpy.core import Infinity
+from simpy.resources.store import StorePut
 
 if TYPE_CHECKING:
     from collections.abc import Generator
 
+    from procsimpy.Line import Line
+    from simpy import Environment
+    from simpy.resources.store import StoreGet
 
-class Exit(StoreNode):
+
+class Exit(Node):
+    """
+    A conceptual Node representing the completion of a process,
+    they are assumed to always be available and have infinite capacity
+    """
+
     def __init__(
         self,
         id: str,
         name: str,
-        *,
-        priority: int = 0,
     ) -> None:
-        super().__init__(id, name, priority=priority)
-        self.exits: list[Entity] = []
+        super().__init__(id, name, capacity=Infinity)
 
-    def run(self) -> Generator:
-        while True:
-            yield self.isRequested
-            assert self.isRequested.value is not None
+    # def initialize(
+    #     self,
+    #     env: Environment,
+    #     line: Line,
+    #     *,
+    #     processes: list[Callable[[Node], Generator]],
+    # ) -> None:
+    #     """
+    #     sets/resets Exit for new simulation
 
-            eventData = self.isRequested.value
-            assert isinstance(eventData, EventData)
-            assert isinstance(eventData.transmission, Entity)
-            assert eventData.time == self.env.now
-            self.isRequested = self.env.event()
-            self.printTrace(isRequested=eventData)
+    #     :param env: SimPy Enviroment
+    #     :type env: Environment
+    #     :param line: Line Orchestration Module
+    #     :type line: LineNode
+    #     :param processes: only here to maintain standard no passthrough allowed
+    #     :type processes: list[Callable[[Node], Generator]]
+    #     """
+    #     # TODO filter to remove process entity instead of not allowing passthrough
+    #     super().initialize(env, line, processes=[])
 
-            self._receive(eventData.transmission)
+    def getToken(self) -> StoreGet:
+        """
+        Wrapper for Node.getToken() that puts an AvailabilityToken into availability,
+          before request, this is because Exits are always available as they are
+            conceptual buckets for the completion of the process
 
-            self.canGiversGive()
+        :return: _description_
+        :rtype: StoreGet
+        """
+        # Exits are always avaible so add Token before request
+        # TODO Check if this will overfill and if that even matters
+        self.availabilityStore.put(self.createToken())
+        return super().getToken()
+
+    def put(self, item: Entity) -> StorePut:
+        """
+        Overload of Node.put required to stop callback of ProcessEntity
+        """
+        self.printTrace(ReceiveEvent(time=self.env.now, caller=self, entity=item))
+
+        item.updateStation(station=self)
+        self.stats.receivedEntity(entity=item)
+
+        return self.store.put(item)
--- procsimpy/Server.py
+++ procsimpy/Server.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from procsimpy.Node import Node
 
+if TYPE_CHECKING:
+    from procsimpy.ProbDistribution import ProbDistribution
 
-class Queue(Node):
+
+class Server(Node):
     def __init__(
         self,
         id: str,
@@ -11,10 +16,12 @@ class Queue(Node):
         *,
         capacity: int = 1,
         priority: int = 0,
+        processingTime: ProbDistribution,
     ) -> None:
         super().__init__(
             id,
             name,
             capacity=capacity,
             priority=priority,
+            processingTime=processingTime,
         )
