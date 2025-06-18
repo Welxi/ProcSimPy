f7257c21c9ce34d3ea2a47daf57105b2594e5d3c
neil <neil.sheahan95@gmail.com>
2025-06-18 13:28:57 +0100

    Update Unit Tests for support features

--- procsimpy/tests/test_printTrace.py
+++ procsimpy/tests/test_printTrace.py
@@ -1,18 +1,26 @@
 import pytest
 
 from procsimpy.Base import Base
-from procsimpy.EventData import EventData
+from procsimpy.Entity import Entity
+from procsimpy.EventData import (
+    CreateEvent,
+    EnterEvent,
+    GiveEvent,
+    InitEvent,
+    ReceiveEvent,
+)
+from procsimpy.Node import Node
 from procsimpy.printTrace import printTrace
 
 
 @pytest.fixture
 def transmission() -> Base:
-    return Base('TE', 'TestEntity')
+    return Entity('TE', 'TestEntity')
 
 
 @pytest.fixture
 def base() -> Base:
-    return Base('Base', 'SimBase')
+    return Node('Base', 'SimBase')
 
 
 @pytest.fixture
@@ -21,85 +29,45 @@ def caller() -> Base:
 
 
 @pytest.fixture
-def eventData(caller, transmission) -> EventData:
-    return EventData(caller=caller, time=1, transmission=transmission)
+def time() -> float:
+    return 1.5
 
 
 @pytest.fixture
-def timecode(eventData) -> str:
-    return f'@T:{eventData.time:>4} ->'
+def timecode(time) -> str:
+    return f'@T:{time:>4.2f} ->'
 
 
-@pytest.mark.skip('Not Implemented')
-def test_printTrace_invalidArg(base, eventData) -> None:
-    with pytest.raises(ValueError, match='Unsupported phrase asdf for printTrace'):
-        printTrace(base=base, asdf=eventData)
-
-
-@pytest.mark.skip('Not Implemented')
-def test_printTrace_TooManArgs(base, eventData) -> None:
-    with pytest.raises(
-        AssertionError, match='Only one phrase per printTrace supported'
-    ):
-        printTrace(base=base, create=eventData, signal=eventData)
-
-
-@pytest.mark.skip('Not Implemented')
-def test_printTrace_keywords(
-    base, eventData, timecode, caller, transmission, capsys
-) -> None:
-    printTrace(base=base, init=eventData)
+def test_CreateEvent(time, caller, timecode, capsys):
+    event = CreateEvent(time=time, caller=caller)
+    printTrace(event)
     captured = capsys.readouterr()
-    assert captured.out == f'{timecode} Object Initialized => {base.name}\n'
+    assert captured.out == f'{timecode} Entity Created => {caller.name}\n'
 
-    printTrace(base=base, create=eventData)
+
+def test_InitEvent(time, caller, timecode, capsys):
+    event = InitEvent(time=time, caller=caller)
+    printTrace(event)
     captured = capsys.readouterr()
-    assert captured.out == f'{timecode} Entity Created => {base.name}\n'
+    assert captured.out == f'{timecode} Object Initialized => {caller.name}\n'
 
-    printTrace(base=base, startWork=eventData)
+
+def test_EnterEvent(time, caller, transmission, timecode, capsys):
+    event = EnterEvent(time=time, caller=transmission, station=caller)
+    printTrace(event)
     captured = capsys.readouterr()
-    assert captured.out == f'{timecode} {base.name} started work\n'
+    assert captured.out == f'{timecode} {transmission.name} entered {caller.name}\n'
 
-    printTrace(base=base, finishWork=eventData)
+
+def test_ReceiveEvent(time, caller, transmission, timecode, capsys):
+    event = ReceiveEvent(time=time, caller=caller, entity=transmission)
+    printTrace(event)
     captured = capsys.readouterr()
-    assert captured.out == f'{timecode} {base.name} finished work\n'
+    assert captured.out == f'{timecode} {caller.name} received {transmission.name}\n'
 
-    printTrace(base=base, interrupted=eventData)
+
+def test_GiveEvent(time, caller, timecode, capsys):
+    event = GiveEvent(time=time, caller=caller)
+    printTrace(event)
     captured = capsys.readouterr()
-    assert captured.out == f'{timecode} {base.name} interrupted by {caller.name}\n'
-
-    printTrace(base=base, interruptEnd=eventData)
-    captured = capsys.readouterr()
-    assert (
-        captured.out == f'{timecode} {caller.name} interruption ended on {base.name}\n'
-    )
-
-    printTrace(base=base, enter=eventData)
-    captured = capsys.readouterr()
-    assert captured.out == f'{timecode} {base.name} entered {caller.name}\n'
-
-    printTrace(base=base, received=eventData)
-    captured = capsys.readouterr()
-    assert captured.out == f'{timecode} {base.name} received {transmission.id}\n'
-
-    printTrace(base=base, gave=eventData)
-    captured = capsys.readouterr()
-    assert captured.out == f'{timecode} {base.name} gave {transmission.id}\n'
-
-
-# def test_printTrace_isRequested(base, eventData, timecode, capsys, context) -> None:
-#     printTrace(base=base, isRequested=eventData)
-#     captured = capsys.readouterr()
-#     assert (
-#         captured.out
-#         == f'{timecode} E:<isRequested> -> {base.name} received an isRequested event for {context.name}\n'
-#     )
-
-
-# def test_printTrace_canDispose(base, eventData, timecode, capsys, context) -> None:
-#     printTrace(base=base, canDispose=eventData)
-#     captured = capsys.readouterr()
-#     assert (
-#         captured.out
-#         == f'{timecode} E:<canDispose> -> {base.name} called from {context.name}\n'
-#     )
+    assert captured.out == f'{timecode} {caller.name} gave Entity\n'
