0e3c86dfaf047d0f20642525730a74dd5b42acde
neil <neil.sheahan95@gmail.com>
2025-06-18 13:28:57 +0100

    Update Unit Tests for support features

--- procsimpy/tests/test_ProbDistribution.py
+++ procsimpy/tests/test_ProbDistribution.py
@@ -14,24 +14,24 @@ def rng() -> Random:
     SEED = 42
     return Random(SEED)
 
-@pytest.mark.skip('Not Implemented')
+
 def test_Fixed(rng) -> None:
     dist = FixedDistribution(mean=1)
     number = dist.distributionFunction(rng)
     assert number == 1
 
-@pytest.mark.skip('Not Implemented')
+
 def test_Normal(rng) -> None:
     dist = GaussianDistribution(mean=1, stdev=0.1, min=0.5, max=1.5)
     number = dist.distributionFunction(rng)
     assert 0.5 < number < 1.5
 
-@pytest.mark.skip('Not Implemented')
+
 def test_Normal_min_greater_than_max() -> None:
-    with pytest.raises(ValueError, match='max must be greater than min'):
+    with pytest.raises(ValueError, match='Does not match a Normal Distribution'):
         GaussianDistribution(mean=1, stdev=0.1, min=1.5, max=0.5)
 
-@pytest.mark.skip('Not Implemented')
+
 def test_Exp(rng):
     dist = ExpDistribution(mean=1)
     number = dist.distributionFunction(rng)
@@ -40,9 +40,9 @@ def test_Exp(rng):
     assert isinstance(number, float)
     assert number < float('inf')
 
-@pytest.mark.skip('Not Implemented')
+
 def test_Exp_invalid_mean() -> None:
-    with pytest.raises(ValueError, match='Mean cannot be less than or equal to 0'):
+    with pytest.raises(ValueError, match='Mean Less or Equal to Zero not supported'):
         ExpDistribution(mean=0)
 
 
--- procsimpy/tests/test_RandomNumberGenerator.py
+++ procsimpy/tests/test_RandomNumberGenerator.py
@@ -1,17 +1,13 @@
-import pytest
-
 from procsimpy.ProbDistribution import FixedDistribution, GaussianDistribution
 from procsimpy.RandomNumberGenerator import RandomNumberGenerator
 
 
-@pytest.mark.skip('Not Implemented')
 def test_RandomNumberGenerator_Fixed() -> None:
     rng = RandomNumberGenerator(FixedDistribution(mean=1))
     number = rng.generateNumber()
     assert number == 1
 
 
-@pytest.mark.skip('Not Implemented')
 def test_RandomNumberGenerator_Normal() -> None:
     dist = GaussianDistribution(mean=1, stdev=0.1, min=0.5, max=1.5)
     rng = RandomNumberGenerator(dist)
